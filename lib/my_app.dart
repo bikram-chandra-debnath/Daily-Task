d1cf817dddc5194455e0b809f5952883c9576967
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2025-12-18 01:26:04 +0600

    Update to a blank app

---
+++ lib/my_app.dart
@@ -0,0 +1,19 @@
+
+import 'package:daily_task/Screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home:  HomeScreen()
+    );
+  }
+}
