67e4f76aa37e43899f809bdedabac2a9cb697116
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-03-27 21:32:07 +0600

    live Date setup

--- lib/view/home/widgets/home_upper_section.dart
+++ lib/view/home/widgets/home_upper_section.dart
@@ -4,12 +4,16 @@ import 'package:daily_task/constants/color.dart';
 import 'package:daily_task/constants/images.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
 
 class UpperSection extends StatelessWidget {
   const UpperSection({super.key});
 
   @override
   Widget build(BuildContext context) {
+    String formattedDate = DateFormat('d MMMM yyyy').format(DateTime.now());
+
+
     return Container(
       height: 222,
       width: double.maxFinite,
@@ -37,19 +41,6 @@ class UpperSection extends StatelessWidget {
                 CustomAppBar(
                   // padding: EdgeInsetsGeometry.zero,
                   title: const Text(
-                    "October 20, 2022",
-                    style: TextStyle(
-                      color: AppColors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  
-                ),
-
-                SizedBox(height: 10),
-
-                Text(
                   "My Todo List",
                   style: TextStyle(
                     fontSize: 32,
@@ -57,6 +48,20 @@ class UpperSection extends StatelessWidget {
                     color: AppColors.white,
                   ),
                 ),
+                  
+                ),
+
+                SizedBox(height: 20),
+                Text(
+                    formattedDate,
+                    style: TextStyle(
+                      color: AppColors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+
+                
               ],
             ),
           ),
